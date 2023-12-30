8f4df8ceb60f91f919461916908ac6353cb34f16
tazeen <tazeenpatel42@gmail.com>
2023-12-30 20:39:50 +0530

    project

--- src/script.js
+++ src/script.js
@@ -27,125 +27,125 @@ menuBtn.addEventListener("click", () => {
 
 
 
-// Jason info
+// Json info
 
 let recipes = [
     {
-        "name": "Veggie Delight",
-        "imageSrc": "https://source.unsplash.com/random?veggies",
-        "time": "30 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (2).png",
+        "time": "40 min",
         "type": "veg",
         "isLiked": true,
         "rating": 4.2
     },
     {
-        "name": "Chicken Grill",
-        "imageSrc": "https://source.unsplash.com/random?chicken",
-        "time": "45 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (2).png",
+        "time": "40 min",
         "type": "non-veg",
         "isLiked": false,
         "rating": 4.5
     },
     {
-        "name": "Cheese Pizza",
-        "imageSrc": "https://source.unsplash.com/random?pizza",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (4).png",
         "time": "40 min",
         "type": "veg",
         "isLiked": true,
         "rating": 4.1
     },
     {
-        "name": "Steak",
-        "imageSrc": "https://source.unsplash.com/random?steak",
-        "time": "60 min",
+        "name":"Russian Salad",
+        "imageSrc": "assets/rounded (4).png",
+        "time": "40 min",
         "type": "non-veg",
         "isLiked": false,
         "rating": 4.7
     },
     {
-        "name": "Grilled Salmon",
-        "imageSrc": "https://source.unsplash.com/random?salmon",
-        "time": "50 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (2).png",
+        "time": "40 min",
         "type": "non-veg",
         "isLiked": true,
         "rating": 4.6
     },
     {
-        "name": "Tomato Pasta",
-        "imageSrc": "https://source.unsplash.com/random?pasta",
-        "time": "35 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (2).png",
+        "time": "40 min",
         "type": "veg",
         "isLiked": false,
         "rating": 4.0
     },
     {
-        "name": "Vegan Salad",
-        "imageSrc": "https://source.unsplash.com/random?salad",
-        "time": "20 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (2).png",
+        "time": "40 min",
         "type": "veg",
         "isLiked": false,
         "rating": 3.9
     },
     {
-        "name": "Fried Chicken",
-        "imageSrc": "https://source.unsplash.com/random?friedChicken",
-        "time": "55 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (3).png",
+        "time": "40 min",
         "type": "non-veg",
         "isLiked": false,
         "rating": 4.3
     },
     {
-        "name": "Mushroom Risotto",
-        "imageSrc": "https://source.unsplash.com/random?risotto",
-        "time": "45 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (5).png",
+        "time": "40 min",
         "type": "veg",
         "isLiked": false,
         "rating": 4.5
     },
     {
-        "name": "Burger",
-        "imageSrc": "https://source.unsplash.com/random?burger",
-        "time": "30 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (4).png",
+        "time": "40 min",
         "type": "non-veg",
         "isLiked": false,
         "rating": 4.2
     },
     {
-        "name": "Paneer Tikka",
-        "imageSrc": "https://source.unsplash.com/random?paneerTikka",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (4).png",
         "time": "40 min",
         "type": "veg",
         "isLiked": false,
         "rating": 4.4
     },
     {
-        "name": "BBQ Ribs",
-        "imageSrc": "https://source.unsplash.com/random?ribs",
-        "time": "70 min",
+        "name": "Russian Salads",
+        "imageSrc": "assets/rounded (4).png",
+        "time": "40 min",
         "type": "non-veg",
         "isLiked": false,
         "rating": 4.6
     },
     {
-        "name": "Caesar Salad",
-        "imageSrc": "https://source.unsplash.com/random?caesarSalad",
-        "time": "25 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (4).png",
+        "time": "40 min",
         "type": "veg",
         "isLiked": false,
         "rating": 3.8
     },
     {
-        "name": "Fish Tacos",
-        "imageSrc": "https://source.unsplash.com/random?fishTacos",
-        "time": "35 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (4).png",
+        "time": "40 min",
         "type": "non-veg",
         "isLiked": false,
         "rating": 4.3
     },
     {
-        "name": "Chocolate Cake",
-        "imageSrc": "https://source.unsplash.com/random?chocolateCake",
-        "time": "90 min",
+        "name": "Russian Salad",
+        "imageSrc": "assets/rounded (4).png",
+        "time": "40 min",
         "type": "veg",
         "isLiked": false,
         "rating": 4.9
